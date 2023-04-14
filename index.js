09d476bc69c5f0903a752de50c9b7bf27a5f051a
Joseph McCafferty <mcafferty.j11@gmail.com>
2023-04-14 17:32:05 -0500

    Lab Completed

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(userLocation){
+    const hq = 42
+    if(userLocation > hq){
+        return userLocation - hq
+    } else if(userLocation < hq){
+        return hq - userLocation
+    }
+}
+
+function distanceFromHqInFeet(userLocation){
+    const feetAway = distanceFromHqInBlocks(userLocation) * 264
+    return feetAway
+}
+
+function distanceTravelledInFeet(travStart, travEnd){
+    if (travStart < travEnd) {
+        return (travEnd - travStart) * 264
+    } else if (travStart > travEnd){
+        return (travStart - travEnd) * 264
+    }
+}
+
+function calculatesFarePrice(travStart, travEnd){
+    const travTotal = distanceTravelledInFeet(travStart, travEnd)
+    if(travTotal < 400){
+        return 0
+    } else if(travTotal > 400 && travTotal < 2000){
+        return (travTotal - 400) * .02 
+    } else if(travTotal > 2000 && travTotal < 2500){
+        return 25
+    } else {
+        return "cannot travel that far"
+    } 
+}
